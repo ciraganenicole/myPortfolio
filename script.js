25de5d675b23fbd08257728c9b1d1996b7e78d11
Ciragane Nicole <ciraganenicole@gmail.com>
2022-02-28 20:16:53 +0100

    Add HTML, CSS && JS linters

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function toogleNavBar() {
+function toogle() {
     const nav = document.getElementById('nav');
     if (nav.style.display !== 'none') nav.style.display = 'none';
     else nav.style.display = 'flex'
 }
+let anchor1 = document.getElementsById('hamb');
+anchor1.onclick = toogle();
+let anchor2 = document.getElementById('hamb1');
+anchor2.onclick = toogle();
